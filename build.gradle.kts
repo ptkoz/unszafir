a68b64baae631cbc83d68966707df646fcebedbf
Patryk Kozlowski <9805525+ptkoz@users.noreply.github.com>
2025-04-17 11:07:58 +0200

    Abandon ideas to compile to native binary

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("application")
     id("java")
-    id("org.graalvm.buildtools.native") version "0.10.6"
-    id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.github.ben-manes.versions") version "0.52.+"
 }
 
 group = "eu.klamrowy"
@@ -18,27 +17,25 @@ application {
 }
 
 dependencies {
-    implementation("info.picocli:picocli:4.7.6")
-    implementation("com.google.inject:guice:7.0.0")
+    implementation("info.picocli:picocli:4.+")
+    annotationProcessor("info.picocli:picocli-codegen:4.+")
+
     implementation("javax.inject:javax.inject:1")
-    implementation("com.googlecode.xades4j:xades4j:2.4.0")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
-    testImplementation(platform("org.junit:junit-bom:5.13.0-M2"))
+    implementation("com.google.dagger:dagger:2.+")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.+")
+
+    implementation("com.googlecode.xades4j:xades4j:2.+")
+
+    testImplementation(platform("org.junit:junit-bom:5.+"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.named<JavaExec>("run") {
-    jvmArgs = listOf("-Ddev=true")
+    jvmArgs = listOf(
+        "-Ddev=true"
+    )
     outputs.upToDateWhen { false }
     standardInput = System.`in`
-}
-
-graalvmNative {
-    binaries {
-        named("main") {
-            imageName.set("unszafir")
-        }
-    }
 }
 
 tasks.test {
